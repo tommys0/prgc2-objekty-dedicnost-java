3a62de0664eab688606f7260b2d7daefa59186bd
tommys0 <149568540+tommys0@users.noreply.github.com>
2024-01-21 10:48:58 +0100

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -105,16 +105,16 @@ class Obdelnik extends Obrazec {
     }
 }
 
-class Obrazce {
+public class Obrazce {
     public static void main(String[] args) {
         Obrazec[] tvary = new Obrazec[5];
-        tvary[0] = new RovnostrannyTrojuhelnik(8);
-        tvary[1] = new Kruh(4.8);
-        tvary[2] = new Ctverec(54);
-        tvary[3] = new Obdelnik(12, 24);
-        tvary[4] = new Obdelnik(10, 10);
+        tvary[0] = new RovnostrannyTrojuhelnik(6);
+        tvary[1] = new Kruh(3.5);
+        tvary[2] = new Ctverec(2);
+        tvary[3] = new Obdelnik(4, 11);
+        tvary[4] = new Obdelnik(5, 5);
 
-        for (Obrazec o : tvary)
+        for (Obrazec o: tvary)
             System.out.println(o);
     }
 }
